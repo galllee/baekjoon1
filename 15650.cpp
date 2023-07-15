4ecab4e97e2ebbbd5a54b1b0c8b3185f179c945e
galllee <kimim0410@gmail.com>
2023-07-15 15:56:48 +0900

    백준 15650

---
+++ 15650.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int n, m;
+int arr[9]={0,};
+bool visited[9]={0,};
+void recursion(int len, int j){
+    if(len==m){
+        for(int i=0; i<m; i++){
+            cout<<arr[i]<<' ';
+        }
+        cout<<'\n';
+        return;
+    }
+    for(int i=j; i<=n; i++){
+        if(!visited[i]){
+            visited[i]=true;
+            arr[len]=i;
+            recursion(len+1, i+1);
+            visited[i]=false;
+        }
+    }
+}
+
+int main(){
+    cin>>n>>m;
+    recursion(0, 1);
+    return 0;
+}
