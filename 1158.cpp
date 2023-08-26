1262e6ff3dbdcffad9558d55517b8aff694ee111
galllee <kimim0410@gmail.com>
2023-08-26 10:51:46 +0900

    백준 1158

---
+++ 1158.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(void)
+{
+    queue <int> q;
+    int n, k;
+
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++)
+        q.push(i);
+    cout << "<";
+    while (!q.empty())
+    {
+        for(int i = 0; i < k - 1; i++)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+        cout << q.front();
+        q.pop();
+        if (!q.empty())
+            cout << ", ";
+    }
+    cout << ">";
+    return (0);
+}
